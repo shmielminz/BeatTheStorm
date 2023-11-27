f0ba425fb372c25e8f91af1c793126264766516d
Shmiel Minz <s0583223395@gmail.com>
2023-11-27 21:54:49 +0000

    Finished web game

---
+++ BeatTheStormApp/BeatTheStormWeb/Pages/gamesettings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeatTheStormWeb.Pages
+{
+    public class gamesettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BeatTheStormApp/BeatTheStormWeb/Pages/help.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeatTheStormWeb.Pages
+{
+    public class helpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
