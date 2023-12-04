115a6d50d457d7f74a2a0a8a7f01d070827d4710
Shmiel Minz <s0583223395@gmail.com>
2023-12-04 19:29:51 +0000

    Started working on jQuery game

---
+++ BeatTheStormApp/BeatTheStormWeb/Pages/gameJquery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeatTheStormWeb.Pages
+{
+    public class gameJqueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
